1016aa5f4aec3b1586c664c1dd20817e3b361ff4
Trang Vu <vulinhtrang2305@gmail.com>
2025-07-04 05:26:31 +0700

    fix homepgae

---
+++ src/components/DashboardContent.js
@@ -0,0 +1,20 @@
+// import React from 'react';
+// import CategoryManager from '../module/CategoryManager';
+// import SpotManager from '../module/SpotManager';
+// // import UserManager from '../modules/UserManager';
+// // import ProvinceManager from '../modules/ProvinceManager';
+
+// const DashboardContent = ({ section }) => {
+//     switch (section) {
+//         case "category":
+//             return <CategoryManager />;
+//         case "spot":
+//             return <SpotManager />;
+//         // case "province":
+//         //     return <ProvinceManager />;
+//         default:
+//             return <div>🚧 Select a module to manage.</div>;
+//     }
+// };
+
+// export default DashboardContent;
